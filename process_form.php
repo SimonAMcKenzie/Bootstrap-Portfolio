a640cd036a5cbf030c905c8abb90b047e73c3b66
Simon_McKenzie <simon-mckenzie@hotmail.co.uk>
2023-10-20 00:22:59 +0100

    new images + added php for form in about section

---
+++ process_form.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+    $to = "simon-mckenzie@hotmail.co.uk"; // Replace with your email address
+    $subject = "New Form Submission from $name";
+    $headers = "From: $email";
+
+    // Send email
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Message sent";
+    } else {
+        echo "Message sending failed";
+    }
+} else {
+    echo "Invalid request";
+}
+?>
